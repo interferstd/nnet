0166f7d3f58dca9553f412e810deef3db3d28dd9
Дмитрий Бычков <dimac090501@hotmail.com>
2019-07-24 14:28:33 +0300

    upd fill

--- fill.cpp
+++ fill.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-	std::cout << "topology: 2 3 1" << std::endl;
+	freopen("testDataRand.txt","w", stdout);
+	std::cout << "topology: 2 300 300 1" << std::endl;
 	for (int i = 2000; i >= 0; i--)
 	{
 		int n1 = (int)(2.0 * rand() / double(RAND_MAX));
@@ -15,4 +16,6 @@ int main()
 		std::cout << "in: " << n1 << ".0 " << n2 << ".0" << std::endl;
 		std::cout << "out: " << t << ".0 " << std::endl;
 	}
+	
+	return 0;
 }
